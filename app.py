484ffa27364c146f7a66de87cab15fe3b418e4ab
Kevin <th3greatsensation@gmail.com>
2020-07-20 20:12:11 +0000

    finished?

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from flask import session
+from flask import redirect
+from flask import url_for
 import random
 import jeopardy
 
 import requests #To access our API
-data = r.json()
 # -- Initialization section --
 app = Flask(__name__)
 
@@ -18,11 +19,50 @@ app.secret_key = b'HO\xf8\xff+\n\x1e\\~/;}'
 # -- Routes section --
 @app.route('/')
 @app.route('/index')
+
 def index():
-    session["name"] = "Anoopa"
+    session["score"] = 0
     return render_template('index.html')
+@app.route('/jeopardy/random')
 def jeopardy_random():
     #Use jservice API/random to get 1 jeopardy clue
-    r = random.randint(jeopardy.data[1])
+    clue = jeopardy.random_clue()
+    session["clue"] = clue
+    return render_template('clue.html')
 
-jeopardy_random()
+@app.route('/results', methods = ['GET', 'POST'])
+def get_results():
+    if request.method == 'GET':
+        return redirect('/jeopardy/random')
+    else:
+        form = request.form
+        answer = form['input_answer'] 
+        if answer == session['clue']['answer']:
+            session['score'] += session['clue']['value']
+        else:
+            session['score'] -= session['clue']['value']    
+    
+        return render_template('results.html')
+
+    #adding answer
+# @app.route('/get_results',methods = ['GET', 'POST'])
+
+# def get_results():
+#     if request.method == 'GET':
+#         return render_template('clue.html')
+#     else: 
+#         form = request.form
+#         data = jeopardy.find_data(['clue_answer'])
+#         return render_template('app.py',data=data)
+
+
+    # answer = clue.input
+    # session["answer"] = answer
+    # if  user_input == session['clue']['answer']:
+    #     session['score'] += session['clue']['value']
+    #     print("Qustion correct!")
+    # else:
+    #     session['score'] += 0
+    #     print("Qustion incorrect!")
+    # return render_template('clue.html')
+
--- jeopardy.py
+++ jeopardy.py
@@ -2,10 +2,7 @@ import requests
 API_endpoint = "https://jservice.io/api/clues"
 API_query ="value=1000"
 API_url = API_endpoint +"?" + API_query
-r = requests.get(API_url)
-data = r.json()
-score = 0
-global clue_answer = clue["answer"]
+
 
 def display_clue(clue):
     clue_category = clue["category"]["title"]
@@ -15,19 +12,40 @@ def display_clue(clue):
     print(f"category: {clue_category}")
     print(f"value: {clue_value}")
     print(f"question: {clue_question}")
-clue_one = data[2]
-display_clue(clue_one)
 
 
-answer = input("answer:")
 
-def current_score ():
-    print("HELLO")
-    if answer == clue_answer:
-        score += clue_value
-    else:
-        score +=0
 
-print(score)
 
-current_score()
+# for clue in clues:
+    
+
+
+# def current_score ():
+#     print("HELLO")
+#     if answer == clue_answer:
+#         score += clue_value
+#     else:
+#         score +=0
+
+# print(score)
+
+# current_score()
+
+def random_clue():
+    API_endpoint = "https://jservice.io/api/random"
+    API_url = API_endpoint +"?"
+    r = requests.get(API_url)
+    data = r.json()
+    print(data)
+    return data[0]
+
+def clue_answer():
+    API_endpoint = "https://jservice.io/api/random"
+    API_url = API_endpoint +"?"
+    r = requests.get(API_url)
+    data = r.json()
+    print(data)
+    return data[0]
+
+
